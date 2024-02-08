7104c28ca2c2847fe4fc8731504fb0d7275c14b8
jxkume <jhonr@metropolia.fi>
2024-02-08 22:43:33 +0200

    first commit

---
+++ src/api/routes/userRoute.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import {
+  checkToken,
+  userDeleteCurrent,
+  userGet,
+  userListGet,
+  userPost,
+  userPutCurrent,
+} from '../controllers/userController';
+import { authenticate } from '../../middlewares';
+import { body } from 'express-validator';
+import { validationErrors } from '../../middlewares';
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(userListGet)
+  .post(
+    body('user_name').isString().isLength({ min: 3 }),
+    body('email').isString().isEmail(),
+    body('password').isString().isLength({ min: 5 }),
+    validationErrors,
+    userPost
+  )
+  .put(authenticate, userPutCurrent)
+  .delete(authenticate, userDeleteCurrent);
+
+router.get('/token', authenticate, checkToken);
+
+router.route('/:id').get(userGet);
+
+export default router;
